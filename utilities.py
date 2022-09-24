a3ed8f6ab345f27d732938af778c350319779bff
Andres <qui18288@uvg.edu.gt>
2022-09-23 19:22:24 -0600

    utils

--- utilities.py
+++ utilities.py
@@ -13,3 +13,7 @@ MINIPROYECTO 4 - SIMULACION SERVIDORES
 import numpy as np
 import random
 import math
+
+
+def exponential_random(lambda_max):
+    return -(1/lambda_max)*(np.log(np.random.uniform()))
